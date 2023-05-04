a78d5a095bad5c89076265d1b01f2e7d468b7301
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-04 21:18:36 +0800

    feat: connect log

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,14 @@ public class Main {
         var app = Javalin.create(/*config*/)
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
+
+        System.out.println("### The Server is Running ###");
+
+        try {
+            connectToDB();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void connectToDB() throws SQLException {
@@ -24,10 +32,10 @@ public class Main {
         try {
             conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" +
                     dbName + "?user=" + userName + "&password=" + password);
-            System.out.println("Connection Success");
+            System.out.println("### Connection Success ###");
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Connection Lost");
+            System.out.println("### Connection Lost ###");
         } finally {
             if (conn != null) {
                 conn.close();
