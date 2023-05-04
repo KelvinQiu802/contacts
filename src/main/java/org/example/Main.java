d21c7f5f0d31e7932b38b775d8a0669ab55eb005
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-04 17:33:19 +0800

    feat: connect to sql

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,36 @@ package org.example;
 
 import io.javalin.Javalin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         var app = Javalin.create(/*config*/)
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
+    }
+
+    public static void connectToDB() throws SQLException {
+        Connection conn = null;
+
+        String host = DBConfig.host;
+        String password = DBConfig.password;
+        String userName = DBConfig.userName;
+        String dbName = DBConfig.dbname;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" +
+                    dbName + "?user=" + userName + "&password=" + password);
+            System.out.println("Connection Success");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Lost");
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 }
