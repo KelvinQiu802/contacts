80ec67d88b69f14c62c32886dfa87279e75ee3a4
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-04 22:36:28 +0800

    feat: cors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.bundled.CorsPluginConfig;
 
 
 public class Main {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create(
+                        config -> {
+                            config.plugins.enableCors(cors -> {
+                                cors.add(CorsPluginConfig::anyHost);
+                            });
+                        }
+                )
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
 
