301b88e2aa2d8478218266485e963df49161332e
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-04 17:33:08 +0800

    fix: add fileds

--- src/main/java/org/example/DBConfigExample.java
+++ src/main/java/org/example/DBConfigExample.java
@@ -2,5 +2,7 @@ package org.example;
 
 public class DBConfigExample {
     public final static String host = "HOST";
+    public final static String dbname = "DB_NAME";
+    public final static String userName = "USER_NAME";
     public final static String password = "PASSWORD";
 }
