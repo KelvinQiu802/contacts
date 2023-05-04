084bb13b09ce75da9f10325a346be61b88e578c8
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-04 22:08:12 +0800

    refactor: remove redundant imports

--- src/main/java/org/example/ContactDAO.java
+++ src/main/java/org/example/ContactDAO.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class ContactDAO {
     public static List<Contact> getAllContacts() {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 import io.javalin.Javalin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
