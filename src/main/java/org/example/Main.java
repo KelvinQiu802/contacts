c1f1effc53f0697b0b0315c6b2452197264c5a59
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-05 10:01:32 +0800

    refactor: api route

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,16 +8,16 @@ import org.example.controller.ContactController;
 public class Main {
     public static void main(String[] args) {
         var app = Javalin.create(
-                        config -> {
-                            config.plugins.enableCors(cors -> {
-                                cors.add(CorsPluginConfig::anyHost);
-                            });
-                        }
-                )
-                .get("/", ctx -> ctx.result("Hello World"))
-                .start(7070);
+                config -> {
+                    config.plugins.enableCors(cors -> {
+                        cors.add(CorsPluginConfig::anyHost);
+                    });
+                }
+        );
 
-        app.get("/contacts", ContactController::allContacts);
+        app.get("/api/contacts", ContactController::allContacts);
+
+        app.start(7070);
 
         System.out.println("### The Server is Running ###");
     }
