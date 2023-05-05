caf5f4c19a06f6790d9a2eeb059201f1b7404f8d
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-05 10:31:44 +0800

    refactor: use try-with-resources

--- src/main/java/org/example/dao/ContactDAO.java
+++ src/main/java/org/example/dao/ContactDAO.java
@@ -14,21 +14,17 @@ import java.util.List;
 public class ContactDAO {
     public static List<Contact> getAllContacts() {
         List<Contact> contacts = new ArrayList<>();
-        Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
 
-        try {
-            conn = DBUtils.connectToDB();
-            st = conn.createStatement();
-            rs = st.executeQuery("SELECT * FROM contacts;");
+        try (
+                Connection conn = DBUtils.connectToDB();
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM contacts;");
+        ) {
             while (rs.next()) {
                 contacts.add(new Contact(rs.getString("Name"), rs.getString("Number")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            DBUtils.closeAll(conn, st, rs);
         }
 
         return contacts;
